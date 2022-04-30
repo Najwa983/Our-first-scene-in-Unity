ffabb409c0329f7121c621ba4b89f99a9202f478
Najwa983 <Najwa.lakmouri1@gmail.com>
2022-04-30 02:06:33 +0100

    Unity project for a 3D scene

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private LayerMask playerMask;
+    [SerializeField] private Material myMat;
+    
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+    }
+    // this methode is called once every physic update
+    private void FixedUpdate()
+    {
+
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
+
+
+
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            Destroy(other.gameObject);
+
+        }
+
+    }
+}
+
+
